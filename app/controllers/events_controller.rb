d9b2a606327893d4e333d0a2d295d4ac8ab4dd12
Yang Li-Yi <joseph.lyyang@gmail.com>
2016-10-30 16:49:36 +0800

    Added edit, update, destroy for events Recipes: Used flash messages Rendered error messages in new and edit page for failed validation Refactored by using before_action, partial form Installed kaminari

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,24 +1,64 @@
 class EventsController < ApplicationController
+  before_action :set_event, :only => [:show, :edit, :update, :destroy]
 
+  # /events/index => index.html.erb
   def index
-    @events = Event.all
+    @events = Event.page(params[:page]).per(5) # [<Event id:1>, <Event id:2>, <Event id:3>...]
   end
 
+  # /events/new => new.html.erb
   def new
     @event = Event.new
   end
 
+  # /events/create => redirect_to index
   def create
-    @event = Event.new(events_params)
+    @event = Event.new(event_params)
 
-    @event.save
+    if @event.save
+      flash[:notice] = "Event is created successfully!"
+
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
+  end
+
+  # /events/show/:id => show.html.erb
+  def show
+  end
+
+  # /events/edit/:id => edit.html.erb
+  def edit
+  end
+
+  # /events/update/:id => redirect to show
+  def update
+    if @event.update(event_params)
+      flash[:notice] = "Event is updated successfully!"
+      redirect_to :action => :show, :id => @event
+    else
+      render :action => :edit
+    end
+  end
+
+  # /events/destroy/:id => redirect to index
+  def destroy
+    @event.destroy
+
+    flash[:alert] = "Event is destroyed!"
+
     redirect_to :action => :index
   end
 
   private
 
-  def events_params
+  def event_params
     params.require(:event).permit(:name, :description)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 
 end
