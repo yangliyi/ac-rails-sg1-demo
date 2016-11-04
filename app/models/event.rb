03e0ca5153e425aada83d2c7b8779d486509a7c2
Yang Li-Yi <joseph.lyyang@gmail.com>
2016-11-04 23:45:50 +0800

    ActiveRecord Associations Practice

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
   validates_presence_of :name
+
+  has_many :attendees
+  belongs_to :category, optional: true
+  has_one :location
+
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
 end
