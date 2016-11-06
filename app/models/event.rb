ec04208ed9097876d24b74b91c435c9b55121234
Yang Li-Yi <joseph.lyyang@gmail.com>
2016-11-06 12:19:12 +0800

    Added Category for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   validates_presence_of :name
 
   has_many :attendees
+  belongs_to :category, optional: true
+
+  delegate :name, :to => :category, :prefix => true,
+ :allow_nil => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105060937) do
+ActiveRecord::Schema.define(version: 20161105090659) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20161105060937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_attendees_on_event_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "events", force: :cascade do |t|
@@ -28,6 +35,8 @@ ActiveRecord::Schema.define(version: 20161105060937) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "status"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_events_on_category_id"
   end
 
   create_table "people", force: :cascade do |t|
